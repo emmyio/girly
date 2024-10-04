07a1f34be63a114a212e59fa76d91ed41304f42a
em <emwishful@gmail.com>
2024-10-04 17:43:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// Custom script to handle hover functionality for audio playback
 document.addEventListener('DOMContentLoaded', () => {
     const volumeControl = document.getElementById('volume');
     const audioElements = document.querySelectorAll('audio');
     let currentAudio = null;
-
     document.querySelectorAll('.friend-section').forEach(section => {
         section.addEventListener('mouseenter', () => {
             const friend = section.getAttribute('data-friend');
             const audioElement = document.getElementById(`${friend}-audio`);
 
             if (currentAudio && currentAudio !== audioElement) {
-                currentAudio.pause(); // Pause the currently playing audio
-                currentAudio.currentTime = 0; // Reset to the beginning
+                currentAudio.pause(); 
+                currentAudio.currentTime = 0;
             }
 
             audioElement.play();
             currentAudio = audioElement;
         });
     });
-
     volumeControl.addEventListener('input', (event) => {
         const volume = event.target.value;
         audioElements.forEach(audio => {
